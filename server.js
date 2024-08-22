39522a94568a878d3002814ec9798962c5667dfe
nabil1986 <kaciminabil@gmail.com>
2024-08-22 21:32:54 +0100

    Initial commit

--- server.js
+++ server.js
@@ -488,7 +488,7 @@ app.get('/typeControle', (req, res) => {
 app.use('/localisationequipement', authenticateJWT);
 
 app.get('/localisationequipement', (req, res) => {
-  const query = 'SELECT localisation FROM localisationequipement ORDER BY ASC';
+  const query = 'SELECT localisation FROM localisationequipement ORDER BY localisation ASC';
   db.query(query, (err, results) => {
     if (err) {
       res.status(500).send(err);
